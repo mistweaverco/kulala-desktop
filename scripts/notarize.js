51d2bb9d9ef9e5a80b55146d437dc797c54bc91f
Marco Kellershoff <marco@mistweaverco.com>
2026-07-10 19:59:15 +0200

    fix(ci): macos notarize

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -15,6 +15,5 @@ exports.default = async function notarizing(context) {
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
-    webhook: process.env.APPLE_NOTARIZATION_WEBHOOK_URL,
   });
 };
